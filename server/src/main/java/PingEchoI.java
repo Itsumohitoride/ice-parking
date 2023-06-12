20d510661b3193f94567fa9e635d8c515f331b6c
Luis Miguel Ossa Arias <luismiguelossaarias05@gmail.com>
2023-06-12 18:08:34 -0500

    The pingEcho class has been implemented

--- server/src/main/java/PingEchoI.java
+++ server/src/main/java/PingEchoI.java
@@ -1,8 +1,26 @@
+import ParkingDemo.PingEcho;
 import com.zeroc.Ice.Current;
 
-public class PingEchoI implements ParkingDemo.PingEcho{
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class PingEchoI implements PingEcho {
     @Override
     public String ping(String ipAddress, Current current) {
-        return null;
+        try {
+            InetAddress inetAddress = InetAddress.getByName(ipAddress);
+
+            if(inetAddress.isReachable(5000)){
+                return "The resource " + ipAddress + " is reachable";
+            }
+
+        } catch (IOException exception){
+            printMessage("Error checking the availability of the resource: " + exception.getMessage());
+        }
+        return "The resource " + ipAddress + " is not reachable";
+    }
+
+    private void printMessage(String message){
+        System.out.println(message);
     }
 }
